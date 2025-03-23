3b6cef8452cc1e34e6a21f1f3388170349751e8c
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-03-23 18:52:05 +0100

    added validation to create message form

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -6,12 +6,26 @@ async function renderCreateMessagesForm(req, res) {
     res.render("create-message-form")
 }
 
+const validateCreateMessageForm = [
+    body("msg")
+        .trim()
+        .isLength({min: 10}).withMessage("Message must be at least 10 characters long")
+]
+
 async function createMessage(req, res) {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).render("create-message-form", {
+             errors: errors.array(),
+         })
+    }
     await db.createMessage(req.body.msg, req.user.id)
     res.redirect("/");
 }
 
 module.exports = {
     renderCreateMessagesForm,
-    createMessage
+    createMessage,
+    validateCreateMessageForm
 }
--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -3,7 +3,7 @@ const messagesController = require("../controllers/messagesController");
 const messagesRouter = Router();
 
 messagesRouter.get("/createMessage",messagesController.renderCreateMessagesForm)
-messagesRouter.post("/createMessage",messagesController.createMessage)
+messagesRouter.post("/createMessage", messagesController.validateCreateMessageForm, messagesController.createMessage)
 
 
 module.exports = messagesRouter
